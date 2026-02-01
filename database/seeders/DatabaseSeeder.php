8a008cbcdf38c50f680dd88d26fe6b5fd75324f2
Iroha71 <y.koba.h1071@outlook.jp>
2026-02-01 21:42:43 +0900

    ユーザーのファクトリーにロールを追加

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
             'password' => 'password',
+            'role' => 0,
         ]);
     }
 }
